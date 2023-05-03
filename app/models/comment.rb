d7bb5f3dfab5cae283ba7ec56e2bafdb7e559cc9
Wai Yan Phyo <ebrain.ebrain@gmail.com>
2023-05-03 23:46:48 +0630

    Add new method in comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,11 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
+
+  class << self
+    def update_comments_counter(post_id)
+      post = Post.find(post_id)
+      post.update(comments_counter: post.comments.count)
+    end
+  end
 end
