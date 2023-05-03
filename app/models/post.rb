0a1c7c4450058a68c046c498fcbfff76cb3996e3
Wai Yan Phyo <ebrain.ebrain@gmail.com>
2023-05-03 23:46:16 +0630

    Add recent comments and like count method in post

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,15 @@ class Post < ApplicationRecord
 
   has_many :comments
   has_many :likes
+
+  class << self
+    def recent_comments(post_id)
+      Comment.where(post_id:).order(created_at: :desc).limit(5)
+    end
+
+    def update_posts_counter(user_id)
+      user = User.find(user_id)
+      user.update(posts_counter: user.posts.count)
+    end
+  end
 end
