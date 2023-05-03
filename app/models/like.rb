c62e211fbdb01fae7f35ba8862cf1c35b9d2da2f
Wai Yan Phyo <ebrain.ebrain@gmail.com>
2023-05-03 23:46:37 +0630

    Add new method in like

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,11 @@
 class Like < ApplicationRecord
   belongs_to :user
   belongs_to :post
+
+  class << self
+    def update_likes_counter(post_id)
+      post = Post.find(post_id)
+      post.update(likes_counter: post.likes.count)
+    end
+  end
 end
