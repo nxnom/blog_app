fa64a2e51a67e20b2190c532bfb449b8ecee20b0
Wai Yan Phyo <ebrain.ebrain@gmail.com>
2023-05-04 01:43:25 +0630

    Change update comment count method

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,10 +2,10 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
 
-  class << self
-    def update_comments_counter(post_id)
-      post = Post.find(post_id)
-      post.update(comments_counter: post.comments.count)
-    end
+  after_save :update_comments_counter
+
+  def update_comments_counter
+    post = Post.find(post_id)
+    post.update(comments_counter: post.comments.count)
   end
 end
