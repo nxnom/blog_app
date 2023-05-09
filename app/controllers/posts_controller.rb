81e0cc3f7b30dfa2ea5130fde705730cb9cda741
Wai Yan Phyo <ebrain.ebrain@gmail.com>
2023-05-09 11:03:24 +0630

    Add actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,9 @@
 class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find_by(id: params['id'], user_id: params['user_id'])
+  end
 end
