8e89f7b461ec45b9ab3bfa5a06306caae529fdb6
Wai Yan Phyo <ebrain.ebrain@gmail.com>
2023-05-11 17:49:10 +0630

    Finish users page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get '/users', to: 'users#index'
-  get '/users/:id', to: 'users#show'
-  get '/users/:user_id/posts', to: 'posts#index'
-  get '/users/:user_id/posts/:id', to: 'posts#show'
+  # get '/users', to: 'users#index', as: 'users'
+  # get '/users/:id', to: 'users#show', as: 'user'
+  # get '/users/:user_id/posts', to: 'posts#index', as: 'user_posts'
+  # get '/users/:user_id/posts/:id', to: 'posts#show', as: 'user_post'
+
+  resources :users, only: %i[index show] do
+    resources :posts, only: %i[index show]
+  end
 
   # Defines the root path route ("/")
   root to: 'users#index'
