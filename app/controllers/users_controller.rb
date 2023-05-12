eed41ce5ab4e592895bba3b1ecf0119b392abe83
Wai Yan Phyo <ebrain.ebrain@gmail.com>
2023-05-12 13:13:12 +0630

    Create new post form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,10 @@ class UsersController < ApplicationController
   def show
     @user = User.find_by(id: params['id'])
   end
+
+  private
+
+  def current_user
+    User.first
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  # get '/users', to: 'users#index', as: 'users'
-  # get '/users/:id', to: 'users#show', as: 'user'
-  # get '/users/:user_id/posts', to: 'posts#index', as: 'user_posts'
-  # get '/users/:user_id/posts/:id', to: 'posts#show', as: 'user_post'
-
   resources :users, only: %i[index show] do
-    resources :posts, only: %i[index show]
+    resources :posts, only: %i[index show new create]
   end
 
   # Defines the root path route ("/")
