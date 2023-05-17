2ce22f30359a7c9ee047ba1157a05f30ae43a009
Wai Yan Phyo <ebrain.ebrain@gmail.com>
2023-05-17 19:14:49 +0630

    Add integration test for user#show page

--- test/system/users_test.rb
+++ test/system/users_test.rb
@@ -34,4 +34,63 @@ class UsersTest < ApplicationSystemTestCase
 
     assert_current_path user_path(users(:ben))
   end
+
+  test 'shows the user\'s profile picture' do
+    visit user_path(users(:iu))
+
+    photo_path = users(:iu).photo
+
+    assert_selector "img[src='#{photo_path}']"
+  end
+
+  test 'shows the user\'s username' do
+    visit user_path(users(:iu))
+
+    assert_text users(:iu).name
+  end
+
+  test 'shows the number of posts the user has written' do
+    visit user_path(users(:iu))
+
+    assert_text "Number of posts: #{users(:iu).posts_counter}"
+  end
+
+  test 'shows the user\'s bio' do
+    visit user_path(users(:iu))
+
+    assert_text users(:iu).bio
+  end
+
+  test 'shows the user\'s first 3 posts' do
+    visit user_path(users(:iu))
+
+    posts = users(:iu).recent_posts
+
+    posts.each do |post|
+      assert_text post.title
+    end
+  end
+
+  test 'shows a button that lets me view all of a user\'s posts' do
+    visit user_path(users(:iu))
+
+    assert_selector 'a', text: 'See all posts'
+  end
+
+  test 'clicking on a user\'s post redirects to that post\'s show page' do
+    visit user_path(users(:iu))
+
+    post = users(:iu).recent_posts.first
+    click_on post.title
+
+    assert_current_path user_post_path(users(:iu), post)
+  end
+
+  test 'clicking to see all posts redirects me to the user\'s post\'s index page' do
+    visit user_path(users(:iu))
+
+    click_on 'See all posts'
+
+    assert_current_path user_posts_path(users(:iu))
+  end
 end
