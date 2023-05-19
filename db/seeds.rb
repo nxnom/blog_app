497eacfeab526cb2cefa781bb4e32f4178b1b4c4
Wai Yan Phyo <ebrain.ebrain@gmail.com>
2023-05-19 15:51:31 +0630

    Add email confirmation

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,8 @@ ben = User.create(
   name: 'Ben',
   bio: 'Lee Eun-young, better known by her stage name Ben, is a South Korean singer and songwriter. She made her official debut in October 2012, with their first extended play 147.5. As of 2020 she released one studio album, five extended plays and fifteen singles',
   photo: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7a/Ben_at_'Bello'_concert_on_January_11%2C_2019_(2).jpg/440px-Ben_at_'Bello'_concert_on_January_11%2C_2019_(2).jpg",
-  reset_password_token: 'ben123'
+  reset_password_token: 'ben123',
+  confirmed_at: Time.now
 )
 
 iu = User.create(
@@ -14,7 +15,8 @@ iu = User.create(
   name: 'IU',
   bio: 'Lee Ji-eun, known professionally as IU, is a South Korean singer-songwriter and actress. She signed with Kakao M in 2007 as a trainee and debuted as a singer at the age of fifteen with her album Lost and Found. Her follow-up albums, Growing Up and IU...IM, brought her mainstream success, but it was through "Good Day", the lead single from her 2010 album Real, that she achieved national stardom. "Good Day" spent five consecutive weeks at the top position of South Korea\'s Gaon Digital Chart, a joint record along with Psy\'s "Gangnam Style".',
   photo: 'https://static.asiachan.com/IU.full.251579.jpg',
-  reset_password_token: 'iu123'
+  reset_password_token: 'iu123',
+  confirmed_at: Time.now
 )
 
 punch = User.create(
@@ -23,7 +25,8 @@ punch = User.create(
   name: 'Punch',
   bio: 'Bae Jin-young, better known by her stage name Punch, is a South Korean singer',
   photo: "https://i1.wp.com/static.wikia.nocookie.net/kpop/images/b/b5/Punch_We're_Breaking_Up_promo_photo_(1).png/revision/latest/scale-to-width-down/1200?cb=20210615234004",
-  reset_password_token: 'punch123'
+  reset_password_token: 'punch123',
+  confirmed_at: Time.now
 )
 
 # Posts
